fe16669e34c42ac7e8acb01c8b5cff71462270e3
Ademicope <84354061+Ademicope@users.noreply.github.com>
2024-10-05 13:16:02 +0100

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager singleton;
+
+    // create array of ground pieces
+    public GroundPiece[] allGroundPieces;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void SetupNewLevel()
+    {
+        allGroundPieces = FindObjectsOfType<GroundPiece>();
+    }
+
+    private void Awake()
+    {
+        if (singleton == null)
+        {
+            singleton = this;
+        }
+        else if (singleton != this)
+        {
+            Destroy(gameObject);
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+    private void OnLevelFinishedLoading(UnityEngine.SceneManagement.Scene scene, LoadSceneMode mode)
+    {
+        SetupNewLevel();
+    }
+
+    public void CheckComplete()
+    {
+        bool isFinished = true;
+
+        for (int i = 0; i < allGroundPieces.Length; i++)
+        {
+            if (allGroundPieces[i].isColored == false)
+            {
+                isFinished = false;
+                break;
+            }
+        }
+
+        if (isFinished)
+        {
+            NextLevel();
+        }
+    }
+
+    private void NextLevel()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 2)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
